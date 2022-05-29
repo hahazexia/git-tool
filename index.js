6afe3864ad6d077f06002b2ca502fe6141961978
zexiazhou <zexiazhou@tencent.com>
2022-05-30 01:58:06 +0800

    feat: est

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const testAction = async (name) => {
             for (const b of res) {
               const mergeRes = await git.merge(b);
               console.log(mergeRes, b, 'mergeRes b');
+              console.log('哈哈哈');
             }
           }
         })
